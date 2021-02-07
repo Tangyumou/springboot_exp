a026431d247d3e7a052d49d7625657da3dbd96dc
tangqinwei <3122094109@qq.com>
2021-02-07 11:19:11 +0800

    optimize websocket serverendpoint.

--- websocket/src/main/java/com/example/websocket/controller/MultiGroupChatWebSocket.java
+++ websocket/src/main/java/com/example/websocket/controller/MultiGroupChatWebSocket.java
@@ -17,91 +17,101 @@ import java.util.concurrent.atomic.AtomicInteger;
 @ServerEndpoint("/chatroom/{roomId}/{userId}")
 @Component
 public class MultiGroupChatWebSocket {
-    private static Map<Integer,MultiGroupChatWebSocket> chatRoomMap = new ConcurrentHashMap<>();
+    // 聊天室服务端
+    private static Map<Integer, MultiGroupChatWebSocket> chatRoomMap = new ConcurrentHashMap<>();
     // 当前在线连接数
     private AtomicInteger onlineCount = new AtomicInteger(0);
     // 所有在线客户端
     private Map<Integer, Session> clients = new ConcurrentHashMap<>();
+    // 聊天室房间号
+    private Integer roomId;
+
     // 连接建立成功
     @OnOpen
     public void onOpen(Session session, @PathParam("roomId") Integer roomId, @PathParam("userId") Integer userId) {
+        this.roomId = roomId;
         MultiGroupChatWebSocket groupChatWebSocket;
-        if(chatRoomMap.containsKey(roomId)) {
-             groupChatWebSocket = chatRoomMap.get(roomId);
-        }else{
+        if (chatRoomMap.containsKey(roomId)) {
+            groupChatWebSocket = chatRoomMap.get(roomId);
+        } else {
             groupChatWebSocket = new MultiGroupChatWebSocket();
-            chatRoomMap.put(roomId,groupChatWebSocket);
-            log.info("新聊天室：{}被创建",roomId);
-            log.info("当前聊天室个数为：{}",chatRoomMap.size());
+            chatRoomMap.put(roomId, groupChatWebSocket);
+            log.info("新聊天室：{}被创建", roomId);
+            log.info("当前聊天室个数为：{}", chatRoomMap.size());
         }
         // 当前房间在线人数
         int onlineNum = groupChatWebSocket.addOnlineCount();
-        groupChatWebSocket.addSession(userId,session);
-        log.info("有新连接{}加入{}聊天室，该聊天室当前在线人数为：{}", userId,roomId, onlineNum);
+        groupChatWebSocket.addSession(userId, session);
+        log.info("有新连接：{}加入聊天室：{}，该聊天室当前在线人数为：{}", userId, roomId, onlineNum);
     }
 
     // 连接关闭
     @OnClose
-    public void onClose(@PathParam("roomId") Integer roomId, @PathParam("userId") Integer userId) {
+    public void onClose(@PathParam("userId") Integer userId) {
         MultiGroupChatWebSocket groupChatWebSocket = chatRoomMap.get(roomId);
         groupChatWebSocket.subOnlineCount();
         groupChatWebSocket.removeSession(userId);
         int onlineNum = groupChatWebSocket.getRoomPeopleNum();
-        log.info("{}房间有一连接关闭：{}，当前在线人数为：{}",roomId,userId,onlineNum);
-        if(onlineNum==0){
+        log.info("聊天室：{}有一连接：{}关闭，当前在线人数为：{}", roomId, userId, onlineNum);
+        if (onlineNum == 0) {
             chatRoomMap.remove(roomId);
-            log.info("{}房间人数为0被关闭",roomId);
-            log.info("当前房间数为{}",chatRoomMap.size());
+            log.info("聊天室：{}人数为0被关闭", roomId);
+            log.info("当前聊天室个数为：{}", chatRoomMap.size());
         }
     }
+
     // 收到客户端消息
     @OnMessage
-    public void onMessage(String message, Session session,@PathParam("roomId") Integer roomId, @PathParam("userId") Integer userId) {
-        log.info("服务端收到房间{}客户端{}的消息：{}",roomId,userId,message);
-        message = userId+":"+message;
-        sendMessage(roomId,message, session);
+    public void onMessage(String message, Session session, @PathParam("userId") Integer userId) {
+        log.info("服务端收到聊天室：{}，客户端：{}的消息：{}", roomId, userId, message);
+        message = userId + ":" + message;
+        sendMessage(message, session);
     }
+
     // 错误处理
     @OnError
-    public void onError(Session session, Throwable error){
+    public void onError(Throwable error) {
         log.error("发生错误");
         error.printStackTrace();
     }
+
     // 群发消息
-    private void sendMessage(Integer roomId,String message,Session session) {
+    private void sendMessage(String message, Session session) {
         MultiGroupChatWebSocket groupChatWebSocket = chatRoomMap.get(roomId);
-        Map<Integer,Session> sessionMap = groupChatWebSocket.getClients();
-        for(Map.Entry<Integer,Session> sessionEntry : sessionMap.entrySet()){
+        Map<Integer, Session> sessionMap = groupChatWebSocket.getClients();
+        for (Map.Entry<Integer, Session> sessionEntry : sessionMap.entrySet()) {
             Session toSession = sessionEntry.getValue();
-            if(!session.equals(toSession)) {
-                log.info("服务端給{}房间客户端{}发送消息{}",roomId,sessionEntry.getKey(),message);
+            if (!session.equals(toSession)) {
+                log.info("服务端给聊天室：{}，客户端：{}发送消息：{}", roomId, sessionEntry.getKey(), message);
                 toSession.getAsyncRemote().sendText(message);
+
             }
         }
     }
-
-    public int addOnlineCount(){
+    // 在线人数加一
+    private int addOnlineCount() {
         return onlineCount.incrementAndGet();
     }
-
-    public int subOnlineCount(){
+    // 在线人数减一
+    private int subOnlineCount() {
         return onlineCount.decrementAndGet();
     }
-
-    public void addSession(Integer userId, Session session){
-        clients.put(userId,session);
+    // 聊天室增加一个客户端
+    private void addSession(Integer userId, Session session) {
+        clients.put(userId, session);
     }
-
-    public void removeSession(Integer userId){
+    // 聊天室减少一个客户端
+    private void removeSession(Integer userId) {
         clients.remove(userId);
     }
-
-    public Map<Integer, Session> getClients() {
+    // 获得该聊天室所有客户端
+    private Map<Integer, Session> getClients() {
         return clients;
     }
-
-    public int getRoomPeopleNum(){
+    // 获得该聊天室在线人数
+    private int getRoomPeopleNum() {
         return onlineCount.get();
     }
+
 
 }
